b3b0b0608c5c35ba55ff9fc51d002d453048910c
Nikky Southerland <nikky@ricksteves.com>
2016-02-17 15:21:45 -0800

    3.5.8

--- lib/cms/version.rb
+++ lib/cms/version.rb
@@ -2,7 +2,7 @@
 # Allows the precise version of BrowserCMS to be determined programatically.
 #
 module Cms
-  VERSION = "3.6.0"
+  VERSION = "3.5.8"
 
   # Return the current version of the CMS.
   def self.version
