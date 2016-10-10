882f4f94d2f5b7ab6646b7ded3bd8c1a4ce44f8b
Bill <bill@ricksteves.com>
2016-10-10 14:43:50 -0700

    Rails 4 Permit attributes

--- app/controllers/cms/content_types_controller.rb
+++ app/controllers/cms/content_types_controller.rb
@@ -3,6 +3,10 @@ require_dependency "cms/application_controller"
 module Cms
   class ContentTypesController < ApplicationController
 
+    def content_type_params
+      params.permit(:name, :group_name, :content_type_group)
+    end
+
     def index
       content_type = ContentType.named(params[:content_type]).first
       @attributes = content_type.orderable_attributes.sort()
--- app/controllers/cms/image_blocks_controller.rb
+++ app/controllers/cms/image_blocks_controller.rb
@@ -1,4 +1,7 @@
 module Cms
   class ImageBlocksController < Cms::ContentBlockController
+    def image_block_params
+      params.permit(:location_ids)
+    end
   end
 end
