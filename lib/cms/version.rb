8d1a8db6ab084153046963b1fedc7534832fe3c2
Bill <bill@ricksteves.com>
2016-10-18 15:12:27 -0700

    Version bump

--- lib/cms/version.rb
+++ lib/cms/version.rb
@@ -2,7 +2,7 @@
 # Allows the precise version of BrowserCMS to be determined programatically.
 #
 module Cms
-  VERSION = "4.0.0.beta.17"
+  VERSION = "4.0.0.beta.18"
 
   # Return the current version of the CMS.
   def self.version
