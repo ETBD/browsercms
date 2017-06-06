d6164a32117f34a48fd9faa2df56cddb856dc753
bill-ricksteves <bill@ricksteves.com>
2017-06-05 17:04:08 -0700

    BCMS Version bump

--- lib/cms/version.rb
+++ lib/cms/version.rb
@@ -2,7 +2,7 @@
 # Allows the precise version of BrowserCMS to be determined programatically.
 #
 module Cms
-  VERSION = "4.0.4"
+  VERSION = "4.0.5"
 
   # Return the current version of the CMS.
   def self.version
