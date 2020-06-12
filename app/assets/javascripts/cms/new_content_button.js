5dee8231cd2c15bfebb973cda16a91f0a4c8a54a
Doug Johnston <doug@ricksteves.com>
2020-06-11 17:48:41 -0700

    Remove unsupported JS

--- app/assets/javascripts/cms/new_content_button.js
+++ app/assets/javascripts/cms/new_content_button.js
@@ -20,9 +20,9 @@ Cms.NewContentButton.prototype.addLinkPath = function(path) {
 Cms.NewContentButton.prototype.updateButtons = function(selectedElement) {
   var nearestSectionId = selectedElement.closest('.nav-folder').find('.nav-list-span:first').data('node-id');
 
-  this.addPagePath(`/cms/sections/${nearestSectionId}/pages/new`);
-  this.addLinkPath(`/cms/sections/${nearestSectionId}/links/new`);
-  this.addSectionPath(`/cms/sections/new?section_id=${nearestSectionId}`);
+  this.addPagePath('/cms/sections/' + nearestSectionId + '/pages/new');
+  this.addLinkPath('/cms/sections/' + nearestSectionId + '/links/new');
+  this.addSectionPath('/cms/sections/new?section_id=' + nearestSectionId);
 };
 
 var newContentButton = new Cms.NewContentButton();
