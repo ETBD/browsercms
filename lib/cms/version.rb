c21f35e50ba196306eae859612a0e74a90e5954e
Doug Johnston <doug@ricksteves.com>
2020-06-15 09:26:11 -0700

    Prepare 4.2.0.beta.5

--- lib/cms/version.rb
+++ lib/cms/version.rb
@@ -2,7 +2,7 @@
 # Allows the precise version of BrowserCMS to be determined programatically.
 #
 module Cms
-  VERSION = "4.2.0.beta.4"
+  VERSION = "4.2.0.beta.5"
 
   # Return the current version of the CMS.
   def self.version
