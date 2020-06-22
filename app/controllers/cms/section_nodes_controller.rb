15701e858944ae2cc190545bcd8e11ebf80d695d
Doug Johnston <doug@ricksteves.com>
2020-06-22 16:37:03 -0700

    Rework how nodes are being updates after a successful AJAX call

--- app/controllers/cms/section_nodes_controller.rb
+++ app/controllers/cms/section_nodes_controller.rb
@@ -26,39 +26,47 @@ module Cms
 
     def move_to_position
       node_to_move = SectionNode.find(params[:id])
-      target_node = SectionNode.find(params[:target_node_id])
       position = params[:position].try(:to_i)
+
+      # The parent folder/section of the node's previous location
+      previous_parent = node_to_move.parent
+
+      # The parent folder/section of the node's new, target location
+      target_parent = SectionNode.find(params[:target_node_id])
 
       # If position is not present, move item to the end of the list
       if position.present?
-        node_to_move.move_to(target_node.node, position)
+        node_to_move.move_to(target_parent.node, position)
       else
-        node_to_move.move_to_end(target_node.node)
+        node_to_move.move_to_end(target_parent.node)
       end
 
+      # Return success. Include all nodes that will need their position and depth updated on the frontend.
       render json: {
         success: true,
         message: %(
-          '#{node_to_move.node.name}' was moved to position ##{position} in '#{target_node.node.name}' folder.
+          '#{node_to_move.node.name}' was moved to position ##{position} in '#{target_parent.node.name}' folder.
         ).squish,
-        updated_values: updated_values(node_to_move, target_node)
+        updated_values: nodes_to_update_on_success(previous_parent, target_parent)
       }
     rescue StandardError => e
       render json: {
         success: false,
         error: e.message,
         message: %(
-          Failed to move '#{node_to_move.node.name}' to position ##{position} in '#{target_node.node.name}' folder.
+          Failed to move '#{node_to_move.node.name}' to position ##{position} in '#{target_parent.node.name}' folder.
         ).squish
       }, status: 500
     end
 
     private
 
-    # Retrieve new positions from all related nodes so that the front-end can be updated.
-    def updated_values(node_to_move, target_node)
-      node_to_move.siblings.not_of_type(Cms::Section::HIDDEN_NODE_TYPES).map { |n| [n.id, n.position, n.depth] } +
-      target_node.siblings.not_of_type(Cms::Section::HIDDEN_NODE_TYPES).map { |n| [n.id, n.position, n.depth] }
+    # Retrieves all siblings that will need updating on success. This includes all siblings
+    # from the node's previous location as well as siblings from the node's new/target location. Also
+    # includes the node itself.
+    def nodes_to_update_on_success(previous_parent, target_parent)
+      (previous_parent.children.not_of_type(Cms::Section::HIDDEN_NODE_TYPES) +
+      target_parent.children.not_of_type(Cms::Section::HIDDEN_NODE_TYPES).uniq).map { |n| [n.id, n.position, n.depth] }
     end
   end
 end
