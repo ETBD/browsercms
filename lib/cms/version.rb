668e85d0d0d0caa4eeed9d291683c5cee95d42fa
Nikky Southerland <nikky@uw.edu>
2021-05-04 16:04:01 -0700

    5.0.1-beta.1

--- lib/cms/version.rb
+++ lib/cms/version.rb
@@ -2,7 +2,7 @@
 # Allows the precise version of BrowserCMS to be determined programatically.
 #
 module Cms
-  VERSION = '5.0.0'
+  VERSION = '5.0.1-beta.1'
 
   # Return the current version of the CMS.
   def self.version
